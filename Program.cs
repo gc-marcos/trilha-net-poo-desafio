d6f357bc9e07c8a0291c72f931d0c21a166c972e
Marcos Carvalho <gc_marcos@hotmail.com>
2024-01-03 15:27:07 -0300

    Trilha .Net
    Todas aplicações solicitadas no desafio trilha-net-poo foram concluídas com sucesso.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("123456789", "NokiaXYZ", "1234567890", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone("987654321", "iPhone12", "1234567890", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
